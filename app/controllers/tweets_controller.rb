e6d201d26b34bf6784bee55adad10f8ddaa3be7b
12dash <soham.dandapath@gmail.com>
2020-07-20 20:51:25 +0800

    Added the token secret configuration

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,17 +7,21 @@ class TweetsController < ApplicationController
 
     def getTweets        
 
+        
+
         client = Twitter::REST::Client.new do |config|
-            config.consumer_key        = "lRKkGdKMinIEGE7I3OgWq8HZP"
-            config.consumer_secret     = "aQn5TW6z9qHM2FIRr9HmP3FJ7HPyxI6BjFLIVSKxfDvkIIcPUi"
-            config.access_token        = "1260907135182897152-kevpG5Ho2LhAAwLx7Sc3fCTQvcxbtb"
-            config.access_token_secret = "AlaU4X1XqCF8h6FzQM7qRfwofYA3GhuT4ROS5p7ZEw6g8"
+            config.consumer_key        = ENV['consumer_key']
+            config.consumer_secret     = ENV['consumer_secret']
+            config.access_token        = ENV['access_token ']
+            config.access_token_secret = ENV['access_token_secret']
         end
 
         client.user("gem")
-
-        @tweets = client.user_timeline('rubyinside', count: 20)    
         
+        @query = "#ruby -rt"
+
+        @tweets = client.search( @query, lang: "ja")
+
         render("tweet")
     end
 
